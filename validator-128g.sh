6071210d2f2abcffa041141a7d44a4e41fe2d0b8
smallmenu <smallmenu@gmail.com>
2026-08-17 12:35:27 +0800

    update: config

--- validator-128g.sh
+++ validator-128g.sh
@@ -66,6 +66,8 @@ exec $VALIDATOR_CMD \
  --account-index program-id \
  --account-index-include-key AddressLookupTab1e1111111111111111111111111 \
  --no-snapshots \
+ --no-snapshot-fetch \
+ --no-incremental-snapshots \
  --maximum-full-snapshots-to-retain 1 \
  --maximum-incremental-snapshots-to-retain 1 \
  --minimal-snapshot-download-speed 10485760 \
@@ -76,7 +78,7 @@ exec $VALIDATOR_CMD \
  --accounts-index-limit minimal \
  --accounts-db-access-storages-method file \
  --accounts-db-cache-limit-mb 8192 \
- --accounts-index-scan-results-limit-mb 2048 \
+ --accounts-index-scan-results-limit-mb 8192 \
  --accounts-db-ancient-append-vecs 300000 \
  --disable-banking-trace \
  --accounts-shrink-ratio 0.90 --accounts-index-bins 2048 \
